47fb4e37e0228959cad409776ae9becf8954a849
MJerich <MatthewJerich@gmail.com>
2022-05-08 10:04:37 -0700

    added product table schema

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,37 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          // this means the comment must be at least one character long
+          len: [1]
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
